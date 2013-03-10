1e7c12276157dc95271e1fa6af2e24048fbe420f
Abhijeet Rastogi <abhijeet.1989@gmail.com>
2013-03-11 00:48:01 +0530

    Alerts are now displayed in table with different columns

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,14 @@ $(function(){
   ws = new WebSocket("ws://localhost:8080");
 
   ws.onmessage = function(evt) {
+    var msg = json_parse(evt.data);
+
+    row = '<tr><td>' + msg.severity + '</td>' + '<td>' + msg.service + '</td>' + '<td>' + msg.description+ '</td>' + '<td>' + msg.server + '</td>'+ '<td>' + msg.source + '</td>' + '<td>' + msg.date + '</td>'  + '<td>' + msg.host_address + '</td>' + '<td>' + msg.additional_info + '</td>' + '<td>' + msg.event_type + '</td>' + '</tr>'
     if ($('#alerts tbody tr:first').length > 0){
-      $('#alerts tbody tr:first').before('<tr><td>' + evt.data + '</td></tr>');
+      $('#alerts tbody tr:first').before(row);
     } else {
-      $('#alerts tbody').append('<tr><td>' + evt.data + '</td></tr>');
+      $('#alerts tbody').append(row);
     }
-    msg = evt.data;
-    var obj = json_parse(msg);
   };
 
   ws.onclose = function() {
